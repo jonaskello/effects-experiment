4d3708e0552aeea4dbbbe835344ace1c3c1d2960
Jonas Kello <jonas.kello@gmail.com>
2020-08-24 15:19:46 +0200

    More fp-ts

--- src/fp-ts/experiment2.ts
+++ src/fp-ts/experiment2.ts
@@ -2,9 +2,11 @@ import * as T from "fp-ts/lib/Task";
 import { sequenceT } from "fp-ts/lib/Apply";
 import { pipe } from "fp-ts/lib/pipeable";
 
-pipe(
+const main = pipe(
   sequenceT(T.task)(T.of(42), T.of("tim")), //[F[A], F[B]] => F[A, B]
   T.map(([answer, name]) =>
     console.log(`Hello ${name}! The answer you're looking for is ${answer}`)
   )
-)();
+);
+
+main();
